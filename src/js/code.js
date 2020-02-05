5480ddce750c25318a96365a07b4f9c4eabf5b2c
Veaceslav <aramexfrontend@mail.ru>
2020-02-05 21:21:47 +0200

    some changes

--- src/js/code.js
+++ src/js/code.js
@@ -1,5 +1,16 @@
 const togleMenu = document.querySelector('.show');
 
+const switchBtn = document.querySelectorAll('.switch');
+
+const activate = document.querySelectorAll('.inactive');
+
+
+
+
 togleMenu.addEventListener('click',function() {
     document.querySelector('.menu__wrapper').classList.toggle('showmenu');
-})
+});
+
+
+
+
